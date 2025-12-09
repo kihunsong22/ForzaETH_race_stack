d49abea9e8ceddb0c5742f697e387642ab105d00
Kihun Song <kiihuns22@gmail.com>
2025-12-09 22:42:09 +0900

    Fix sync_to_shared script

--- sync_to_shared.sh
+++ sync_to_shared.sh
@@ -5,28 +5,26 @@ DEST_DIR=~/shared_dir/race_stack
 
 echo "=== Syncing modified packages to shared_dir ==="
 
-# Delete everything and recreate
+# 1. 목적지 초기화
 echo "Cleaning destination..."
 rm -rf "$DEST_DIR/"
 
-# Create necessary subdirectories
+# 2. 필수 하위 폴더 생성
 mkdir -p "$DEST_DIR/packages"
 mkdir -p "$DEST_DIR/utilities/libraries"
-mkdir -p "$DEST_DIR/stack_master/config"
-mkdir -p "$DEST_DIR/stack_master/launch"
-mkdir -p "$DEST_DIR/stack_master/maps/small_hall"
 
-# Copy modified packages
+# 3. 패키지 복사
 echo "Copying packages..."
 cp -r state_machine "$DEST_DIR/"
 cp -r controller "$DEST_DIR/"
 cp -r packages/overtake_section_detector "$DEST_DIR/packages/"
 cp -r utilities/libraries/f110_msgs "$DEST_DIR/utilities/libraries/"
+cp -r stack_master "$DEST_DIR/"
 
-# Copy modified configs
-echo "Copying configs..."
-cp stack_master/config/state_machine_params.yaml "$DEST_DIR/stack_master/config/"
-cp stack_master/launch/head_to_head_launch.xml "$DEST_DIR/stack_master/launch/"
-cp stack_master/maps/small_hall/ot_sectors.yaml "$DEST_DIR/stack_master/maps/small_hall/"
+# 빌드 충돌 방지를 위해 불필요한 폴더 제거
+rm -rf "$DEST_DIR/stack_master/build"
+rm -rf "$DEST_DIR/stack_master/install"
+rm -rf "$DEST_DIR/stack_master/log"
+rm -rf "$DEST_DIR/stack_master/__pycache__"
 
 echo "=== Sync complete! ==="
